8c3ae8e72ded56d094f0c7b591fbc705da3f002c
kelly_maass <kmaass77627@gmail.com>
2020-11-23 14:26:20 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
           JSX lets us include JavaScript functions right along with the HTML, and also
           allows us to add in components, which are separate, self-contained chunks of JSX.
         </p>
-        <ExampleComponent />
+        <ExampleComponent /><ExampleComponent />
 
       </div>
     );
